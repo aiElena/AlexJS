e55196378641148910a6ca0857f6a4a8eff8440a
aiElena <ai.alexandrova@ukr.net>
2020-01-17 16:39:00 +0200

    js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 
 let str = "some test string";
   
-  document.getElementById("out_num").innerHTML = str[0];
-  document.getElementById("out_num").innerHTML = str[str.length - 1];
+  document.getElementById("out_num").innerHTML = str[0, str.length - 1];
+  
   
 
  
