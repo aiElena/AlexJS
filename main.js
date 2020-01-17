9c96538c2229564dd175cd336bb919dd9c54257b
aiElena <ai.alexandrova@ukr.net>
2020-01-17 22:55:26 +0200

    js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let str = "some test string";
   
   document.getElementById("out_num").innerHTML = str.charAt(0) + ', ' + str.charAt(str.length - 1);
   
-  document.getElementById("out_num1").innerHTML = str.charAt(0).toUpperCase() + ', ' + str.charAt(str.length - 1).toUpperCase();
+  document.getElementById("out_num1").innerHTML = str.toUpperCase(0, str.length - 1);
  
 
   
