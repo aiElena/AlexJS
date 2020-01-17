c63819c65955f669dd3284aad1746d2eba7d175b
aiElena <ai.alexandrova@ukr.net>
2020-01-17 22:36:24 +0200

    js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
 let str = "some test string";
   
+  document.getElementById("out_num").innerHTML = str.charAt(0);
+  document.getElementById("out_num1").innerHTML = str.charAt(str.length - 1);
 
-  document.getElementById("out_num").innerHTML = str.split(1, str.length - 1);
- 
   
   
   
