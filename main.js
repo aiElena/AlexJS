1b7aa83309220a9ca413c22bceef4d7b231976b9
aiElena <ai.alexandrova@ukr.net>
2020-01-17 22:08:45 +0200

    js,css

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 
 let str = "some test string";
   
-  document.getElementById("out_num").innerHTML = str.charAt(0). + .', '. + .str.charAt(str.length - 1);
+  document.getElementById("out_num").innerHTML = str.charAt(0);
+  document.getElementById("out_num1").innerHTML = str.charAt(str.length - 1);
   
   
 
