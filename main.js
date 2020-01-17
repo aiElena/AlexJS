7d74e517e71ee84dd31eb9eeb5b9172f844bbc43
aiElena <ai.alexandrova@ukr.net>
2020-01-17 17:05:11 +0200

    js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 let str = "some test string";
   
-  document.getElementById("out_num").innerHTML = str.slice(0, 1);
+  document.getElementById("out_num").innerHTML = str.slice(0, 1). ', '.str.slice(1, 1);
   
   
 
