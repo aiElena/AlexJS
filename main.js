65a5e6a56d38fa8266d8de9628a306cee4c7ba43
aiElena <ai.alexandrova@ukr.net>
2020-01-17 22:44:08 +0200

    js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 let str = "some test string";
   
-  document.getElementById("out_num").innerHTML = str.charAt(0) + str.charAt(str.length - 1);
+  document.getElementById("out_num").innerHTML = str.charAt(0) + ', ' + str.charAt(str.length - 1);
  
 
   
