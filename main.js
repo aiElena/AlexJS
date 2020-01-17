8c94d6e5fb1ffa76b0e36edde1862c71e4380802
aiElena <ai.alexandrova@ukr.net>
2020-01-17 22:52:29 +0200

    js

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 let str = "some test string";
   
   document.getElementById("out_num").innerHTML = str.charAt(0) + ', ' + str.charAt(str.length - 1);
+  
+  document.getElementById("out_num1").innerHTML = str.charAt(0).toUpperCase() + ', ' + str.charAt(str.length - 1).toUpperCase();
  
 
   
