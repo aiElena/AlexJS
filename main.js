277258b9a320259f98a5c1d12575738f6e6533d5
aiElena <ai.alexandrova@ukr.net>
2020-01-17 22:29:10 +0200

    js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 
 let str = "some test string";
   
-  document.getElementById("out_num").innerHTML = str.charAt(0);
-  document.getElementById("out_num1").innerHTML = str.charAt(str.length - 1);
+  document.getElementById("out_num").innerHTML = padStart(str.charAt(0) [, str.charAt(str.length - 1)]);
+ 
+  
+
   
   
 
