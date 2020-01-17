5e59533f37a52152003f42e2a46b735351116df6
aiElena <ai.alexandrova@ukr.net>
2020-01-17 23:11:50 +0200

    js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let str = "some test string";
   
   document.getElementById("out_num").innerHTML = str.charAt(0) + ', ' + str.charAt(str.length - 1);
   
-  document.getElementById("out_num1").innerHTML = str.charAt(0).concat(" ", str.toLowerCase(1, str.length - 2));
+  document.getElementById("out_num1").innerHTML = str.toUpperCase.charAt(0).concat("", str.toLowerCase(1, str.length - 2));
  
 
   
