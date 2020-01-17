36311ec124b77cdff169c42dc5556081150066d7
aiElena <ai.alexandrova@ukr.net>
2020-01-17 14:43:30 +0200

    js

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
-
+document.getElementById('out_num').innerHTML('hello world Текст 1');
 
   let out_num = document.getElementById('out_num');
   out_num.innerHTML = (x);
  
 
-let x = '*';
-let out_num = document.getElementById('out_num');
-for (let i = 1; i < 5; i++) {
-  document.write(x);
-}
-out_num.innerHTML = (x);
+
 
