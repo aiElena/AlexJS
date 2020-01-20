379c6d85d6924790dfb89e3ad3ea06a1ce6165d3
aiElena <ai.alexandrova@ukr.net>
2020-01-20 07:13:12 +0200

    js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let str = "some test string";
   
   document.getElementById("out_num1").innerHTML = str[0].toUpperCase() + str.slice(1, str.length - 2) + str[str.length - 1].toUpperCase();    
  
-  document.getElementById("out_num2").innerHTML = str.indexOf('string');
+  document.getElementById("out_num2").innerHTML = pos.str.indexOf('string');
   
   
   
