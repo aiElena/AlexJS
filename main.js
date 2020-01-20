e94f622ae951b04a65578f28b95b39113630fada
aiElena <ai.alexandrova@ukr.net>
2020-01-20 08:07:35 +0200

    js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
 let str = "some test string";
   
-  document.getElementById("out_num").innerHTML = str.charAt(0) + ', ' + str.charAt(str.length - 1);
+  document.getElementById("out_num1").innerHTML = str.charAt(0) + ', ' + str.charAt(str.length - 1);
   
-  document.getElementById("out_num1").innerHTML = str[0].toUpperCase() + str.slice(1, str.length - 2) + str[str.length - 1].toUpperCase();
+  document.getElementById("out_num2").innerHTML = str[0].toUpperCase() + str.slice(1, str.length - 2) + str[str.length - 1].toUpperCase();
   
  
-  document.getElementById("out_num2").innerHTML = str.indexOf('string');
+  document.getElementById("out_num3").innerHTML = str.indexOf('string');
  
   
   
