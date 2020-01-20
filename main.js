cb16f1c811b1772c8d1efc208df041b0258213ea
aiElena <ai.alexandrova@ukr.net>
2020-01-20 08:21:25 +0200

    js

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 
-let str = "some test string";
+	let str = "some test string";
   
-  document.getElementById("out_num1").innerHTML = str.charAt(0) + ', ' + str.charAt(str.length - 1);
+    document.getElementById("out_num1").innerHTML = str.charAt(0) + ', ' + str.charAt(str.length - 1);
   
-  document.getElementById("out_num2").innerHTML = str[0].toUpperCase() + str.slice(1, str.length - 2) + str[str.length - 1].toUpperCase();
+    document.getElementById("out_num2").innerHTML = str[0].toUpperCase() + str.slice(1, str.length - 2) + str[str.length - 1].toUpperCase();
   
  
-  document.getElementById("out_num3").innerHTML = str.indexOf('string');
+    document.getElementById("out_num3").innerHTML = str.indexOf('string');
  
-  
-  
+	let a = "20";
+	let b = "16";
+	let string = 0;
+    document.getElementById("out_num8").innerHTML =	string.concat( a, b);
+ 
 
 
   
