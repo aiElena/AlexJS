e93b42ecee9014863cd233bcf1a32800ba758270
aiElena <ai.alexandrova@ukr.net>
2020-01-20 07:45:10 +0200

    js

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 
-let str = "some test  string";
+let str = "some test string";
   
   document.getElementById("out_num").innerHTML = str.charAt(0) + ', ' + str.charAt(str.length - 1);
   
   document.getElementById("out_num1").innerHTML = str[0].toUpperCase() + str.slice(1, str.length - 2) + str[str.length - 1].toUpperCase();
   
  
-  document.getElementById("out_num2").innerHTML = str.indexOf('string'); 
+  document.getElementById("out_num2").innerHTML = str.indexOf('string');
+document.write(str.indexOf('string'));  
   
   
 
