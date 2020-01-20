c52346b21bf8ec9724f394bebe9b516023a81076
aiElena <ai.alexandrova@ukr.net>
2020-01-20 07:18:34 +0200

    js

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ let str = "some test string";
   
   document.getElementById("out_num").innerHTML = str.charAt(0) + ', ' + str.charAt(str.length - 1);
   
-  document.getElementById("out_num1").innerHTML = str[0].toUpperCase() + str.slice(1, str.length - 2) + str[str.length - 1].toUpperCase();    
- 
-  document.getElementById("out_num2").innerHTML = pos.str.indexOf('string');
+  document.getElementById("out_num1").innerHTML = str[0].toUpperCase() + str.slice(1, str.length - 2) + str[str.length - 1].toUpperCase();
+  
+let result = str.match('string'); 
+  document.getElementById("out_num2").innerHTML = result.length;
   
   
   
