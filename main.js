2f6d1b59fe3fde85c11296ca28c787251e0fa2dd
aiElena <ai.alexandrova@ukr.net>
2020-01-20 07:33:30 +0200

    js

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ let str = "some test string";
   
   document.getElementById("out_num1").innerHTML = str[0].toUpperCase() + str.slice(1, str.length - 2) + str[str.length - 1].toUpperCase();
   
-let result = str.match("string"); 
-  document.getElementById("out_num2").innerHTML = result.length;
+ 
+  document.getElementById("out_num2").innerHTML = str.indexOf('string'); 
   
+  
+
 
   
   
